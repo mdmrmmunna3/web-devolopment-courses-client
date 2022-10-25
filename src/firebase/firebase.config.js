078ee716388e87bc1eb3dfa7cca97a8f98345767
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-10-25 15:58:38 +0600

    responsive navbar added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCB-xpbmPZCeofKqXafuEXyuP0-QRaz1Dc",
+//   authDomain: "wd-development-course.firebaseapp.com",
+//   projectId: "wd-development-course",
+//   storageBucket: "wd-development-course.appspot.com",
+//   messagingSenderId: "757565895435",
+//   appId: "1:757565895435:web:2c30662fdbb50e95eee80e"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export default app ;
---
+++ src/shared/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/logo/logo.png'
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container >
+                <Navbar.Brand>
+                    <Link to='/' className='text-decoration-none text-white d-flex align-items-center'>
+                    <img
+                        alt=""
+                        src={logo}
+                        width="50"
+                        height="50"
+                        className="d-inline-block align-top rounded me-2" /> 
+                        <span>WD-Development-Course</span>
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink className='text-decoration-none text-white me-2 fw-bold'>Courses</NavLink>
+                        <NavLink className='text-decoration-none text-white me-2 fw-bold'>FAQ</NavLink>
+                        <NavLink className='text-decoration-none text-white me-2 fw-bold'>Blog</NavLink>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
