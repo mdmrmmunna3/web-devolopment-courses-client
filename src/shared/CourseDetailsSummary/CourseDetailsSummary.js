13e72d85c4851a6bd372e1bc7232e4f7bf7dc49b
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-10-26 14:08:03 +0600

    get enroll btn and rating add

--- src/shared/CourseDetailsSummary/CourseDetailsSummary.js
+++ src/shared/CourseDetailsSummary/CourseDetailsSummary.js
@@ -2,27 +2,41 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
-import './CourseDetailsSummary.css'
+import './CourseDetailsSummary.css';
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 const CourseDetailsSummary = ({ course }) => {
-    const { id, name , img, title, desciption} = course
+    const { id, name, img, title, desciption, rating } = course
     return (
-        <Card className='card-container'>
-            <Card.Header className="text-center text-white" style={{backgroundColor: '#212529'}}>{name}</Card.Header>
+        <Card className='card-container shadow'>
+            <Card.Header className="text-center text-white" style={{ backgroundColor: '#212529' }}>{name}</Card.Header>
             <Card.Img variant="top" src={img} />
             <Card.Body>
-                <Card.Title>{ title}</Card.Title>
+                <Card.Title>{title}</Card.Title>
                 <Card.Text>
                     <>Description : {
-                        desciption?.length > 200 ? 
-                        <>{desciption.slice(0, 200) + '...'} <Link to={`/courses/${id} `} className='text-decoration-none fw-semibold'>Read More</Link> </>
-                        :
-                        desciption
+                        desciption?.length > 200 ?
+                            <>{desciption.slice(0, 200) + '...'} <Link to={`/courses/${id} `} className='text-decoration-none fw-semibold'>Read More</Link> </>
+                            :
+                            desciption
                     }</>
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <div className='d-flex justify-content-between mt-2'>
+                <Card.Footer className="border-0 text-dark">
+                    <div className='d-flex align-items-center'>
+                        <span className='me-2 text-warning'>
+                            <FaStar></FaStar>
+                            <FaStar></FaStar>
+                            <FaStar></FaStar>
+                            <FaStar></FaStar>
+                            <FaStarHalfAlt></FaStarHalfAlt>
+                        </span>
+                        <span className='fw-semibold'>{rating.number}</span>
+                    </div>
+                </Card.Footer>
+                <Button variant="outline-primary border-0" className=' fw-bold' >Get Enroll</Button>
+            </div>
         </Card>
     );
 };
